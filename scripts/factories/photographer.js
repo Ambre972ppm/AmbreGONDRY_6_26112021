043c1a3f9dc5f5c9d859fc2be27a21e1ca0cace9
Ambre GONDRY <gondry.ambre@gmail.com>
2022-02-09 14:10:53 +0100

    Sass installation + display photographers on home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,42 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `/assets/photographers/${portrait}`;
+    const location = `${city}, ${country}`;
+    const dailyRate =  `${price}€/jour`;
+
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const photograherCard = document.createElement( 'aside' ); // création du container
+
+        const photographerLink = document.createElement('a'); // création du lien vers le profile du photographe
+        photographerLink.setAttribute('href', `/photographer.html?id=${id}`); // définition de l'url
+        photograherCard.appendChild(photographerLink); // définition du lien comme enfant du container
+
+        const profilePicture = document.createElement( 'img' ); // création de la photo de profil
+        profilePicture.setAttribute("src", picture); // définition de l'image
+        profilePicture.setAttribute("alt", `${name}`); // définition du nom de l'image
+        photographerLink.appendChild(profilePicture); // est l'enfant du lien du profil
+
+        const profileName = document.createElement( 'h2' ); // création du nom du profil
+        profileName.textContent = name; // on affiche le nom sous forme de texte
+        photographerLink.appendChild(profileName); // est l'enfant du lien du profil
+
+        const profileLocation = document.createElement( 'h3' ); // création de la localisation du photographe
+        profileLocation.textContent = location; // on l'affiche sous forme de texte
+        photographerLink.appendChild(profileLocation); // est l'enfant du lien
+
+        const profileQuote = document.createElement( 'p' ); // création de la citation
+        profileQuote.setAttribute('class', "quote");
+        profileQuote.textContent = tagline; // qu'on affiche sous forme de texte
+        photographerLink.appendChild(profileQuote); // est l'enfant du lien
+
+        const profileRate = document.createElement( 'p' ); // création du tarif
+        profileRate.setAttribute('class', "rate");
+        profileRate.textContent = dailyRate; // affichage sous forme de texte
+        photographerLink.appendChild(profileRate); // enfant du lien
+
+        return (photograherCard); // on retourne la carte du photographe
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, tagline, location,  getUserCardDOM }
 }
